bf8988fb740f32a7b5ddb016d05f4e5963756632
yiwoori <yiwoori93@naver.com>
2021-06-23 00:46:29 +0900

    Update Order.java

--- Vending/src/Ven/Order.java
+++ Vending/src/Ven/Order.java
@@ -79,7 +79,7 @@ public class Order {
 					Integer.parseInt(orderData[3]));
 			
 			int result = dao.editProductList(conn, pList);
-			
+		 	
 			if(result > 0) {
 				System.out.println("주문 완료");
 			} else {
