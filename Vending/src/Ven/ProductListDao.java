09bc8e0132649a5db158581cb41dedb8d1716b06
doyongsung <ppelpp@naver.com>
2021-06-22 17:25:33 +0900

    Revert "Merge branch 'arum' into MainSuB"
    This reverts commit 1c01295ba7591ca7f1b3bcb62221c1904866ba2c, reversing
    changes made to fc8f3dcd72fb1f7e6c9d7deedc03337cf33af1a3.

--- Vending/src/Ven/ProductListDao.java
+++ Vending/src/Ven/ProductListDao.java
@@ -1,63 +1,58 @@
 package Ven;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
 public class ProductListDao {
-
-	// 싱글톤 패턴 : 여러개의 인스턴스를 생 성하지 못하도록 하는 디자인 패턴
-	// 1. 외부 클래스 또는 인스턴스에서 해당 클래스로 인스턴스를 생성하지 못하도록 처리
-	private ProductListDao(){
-	}
 	
-	// 2. 클래스 내부에서 인스턴스를 만들고 
-	static private ProductListDao dao = new ProductListDao(); 
-	
-	// 3. 메소드를 통해서 반환 하도록 처리
-	public static ProductListDao getInstance() {
-		return dao;
-	}
-	
-	
-	// 1. 전체 데이터 검색 기능
-	// 반환 타입 List<Dept>
-	// 매개변수 - Connection 객체 : Statement
-	ArrayList<ProductList> getProductList(Connection conn) {
-
-		ArrayList<ProductList> list = null;
-
-		// 데이터 베이스의 ITEMINFO 테이블 이용 select 결과를 -> list 저장
-		Statement stmt = null;
-		ResultSet rs = null;
-
-		try {
+	//싱글톤 패턴
+     private ProductListDao() {
+     }
+     
+     //클래스 내부에 인스턴스
+     static private ProductListDao dao = new ProductListDao();
+     
+     //메소드를 통해 반환
+     public static ProductListDao getInstance() {
+    	 return dao;
+     }
+     
+     
+     //전체 타입 검색기능
+     //반환타입 List<productList>
+     //매개변수 - Connection 객체 : Statement
+     ArrayList<ProductList> getProductList(Connection conn){
+    	 
+    	 ArrayList<ProductList> list = null;
+    	
+    	 // 데이터 베이스의 ProductList 테이블 이용 select 결과물 -> list 에 저장
+    	 Statement stmt = null;
+    	 ResultSet rs = null;
+    	 
+    	 try {
 			stmt = conn.createStatement();
-			String sql = "select * from iteminfo order by itemcode";
-
-			// 결과 받아오기
+			String sql = "select * from ProductList";
+			
+			//결과 받아오기
 			rs = stmt.executeQuery(sql);
-
+			
 			list = new ArrayList<>();
-
-			// 데이터를 ITEMINFO 객체로 생성 -> list에 저장
-			while (rs.next()) {
+			
+			//데이터를 ProductList 객체로 생성 -> list에 저장
+			
+			while(rs.next()) {
+				ProductList PL = new ProductList(rs.getInt(1),rs.getString(2),rs.getInt(3),rs.getInt(4));
 				
-				ProductList d = new ProductList(rs.getInt(1),rs.getString(2),rs.getInt(3),rs.getInt(4));
-				
-				list.add(d);
+				list.add(PL);
 			}
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-
-		} finally {
-			
+		}finally {
 			if(rs != null) {
 				try {
 					rs.close();
@@ -66,8 +61,7 @@ public class ProductListDao {
 					e.printStackTrace();
 				}
 			}
-			
-			if (stmt != null) {
+			if(stmt != null) {
 				try {
 					stmt.close();
 				} catch (SQLException e) {
@@ -75,11 +69,9 @@ public class ProductListDao {
 					e.printStackTrace();
 				}
 			}
-
 		}
-
+    	 
 		return list;
-	}
+    	 
+     }
 }
-	
-	
