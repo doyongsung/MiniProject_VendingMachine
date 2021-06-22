354a4c818bcbaa48d581c01a2e339ac4da692ace
yiwoori <yiwoori93@naver.com>
2021-06-22 17:40:41 +0900

    Create TestManager.java

---
+++ Vending/src/Ven/TestManager.java
@@ -0,0 +1,19 @@
+package Ven;
+
+import java.util.Scanner;
+
+public class TestManager {
+
+
+	private ProductListDao dao;
+	private Scanner sc;
+	
+	public DeptManager(DeptDao dao) {
+		this.dao = dao;
+		sc = new Scanner(System.in);
+	}
+	
+	
+	
+	
+}
