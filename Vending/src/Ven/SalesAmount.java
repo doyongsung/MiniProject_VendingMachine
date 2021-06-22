b4666a524e2cb2f4270b1ba612afe3bf3358c968
kimrumm <83346234+kimrumm@users.noreply.github.com>
2021-06-22 14:54:18 +0900

    Update SalesAmount.java

--- Vending/src/Ven/SalesAmount.java
+++ Vending/src/Ven/SalesAmount.java
@@ -3,7 +3,8 @@ package Ven;
 import java.util.Scanner;
 
 public class SalesAmount {
-	
+	// 총매출리스트 -> 높은판매순서별로 나열
+	// 사용자에게 입력받은 상품의 매출만 출력
 
 	public static void main(String[] args) {
 	Scanner num = new Scanner(System.in);
