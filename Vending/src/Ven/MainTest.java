b2a8d637c10b8ee23fe4ae3be5eb11b89a809ba3
doyongsung <ppelpp@naver.com>
2021-06-22 20:54:53 +0900

    UpdateMain

--- Vending/src/Ven/MainTest.java
+++ Vending/src/Ven/MainTest.java
@@ -2,49 +2,32 @@ package Ven;
 
 import java.util.Scanner;
 
+
 public class MainTest {
 public static void main(String[] args) {
-	
 	ProductManager manager = new ProductManager(ProductListDao.getInstance());
-	
+
 	Scanner sc = new Scanner(System.in);
-	try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
+
 		
-		while(true) {
-			System.out.println("자판기 프로그램");
-			System.out.println("-------------------------------");
-			System.out.println("1.음료 리스트");
-			System.out.println("2.음료 정보 등록");
-			System.out.println("3.음료 수정");
-			System.out.println("4.음료 정보 삭제");
-			System.out.println("5.프로그램 종료");
-			System.out.println("-----------------------------------");
-			System.out.println("원하시는 기능의 번호를 입력해주세요.");
-			int num = Integer.parseInt(sc.nextLine());
-		
-		switch(num) {
+		System.out.println("1. 음료 구매     2. 관리자. 3. 프로그램 종료");
+		int choice = Integer.parseInt(sc.nextLine());
+		switch (choice) {
 		case 1:
-			manager.itemList();
+			
 			break;
 		case 2:
-			manager.inputData();
-			break;			
+			manager.buyShowinfo(choice);			
+			manager.saleshowinfo();
 		case 3:
-			manager.editItem();
-			break;
-		case 4:
-			break;
-		case 5:
 			System.out.println("프로그램을 종료합니다.");
-			return;
+			break;
 		}
-		}
+		
+		
 	
 		
-	} catch (ClassNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		
+		   
 	}
-}
 }
--- Vending/src/Ven/ProductManager.java
+++ Vending/src/Ven/ProductManager.java
@@ -113,9 +113,67 @@ public class ProductManager {
 			e.printStackTrace();
 		}
 		
-		
 	}
 
+	   void saleshowinfo() {
+		   ProductManager manager = new ProductManager(ProductListDao.getInstance());
+			
+			
+			Scanner sc = new Scanner(System.in);
+			
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				
+	while(true) {
+		System.out.println("자판기 프로그램");
+		System.out.println("-------------------------------");
+		System.out.println("1.음료 리스트");
+		System.out.println("2.음료 정보 등록");
+		System.out.println("3.음료 수정");
+		System.out.println("4.음료 정보 삭제");
+		System.out.println("5.프로그램 종료");
+		System.out.println("-----------------------------------");
+		System.out.println("원하시는 기능의 번호를 입력해주세요.");
+		int num = Integer.parseInt(sc.nextLine());
 	
+	switch(num) {
+	case 1:
+		manager.itemList();
+		break;
+	case 2:
+		manager.inputData();
+		break;			
+	case 3:
+		manager.editItem();
+		break;
+	case 4:
+		break;
+	}
+	}
+	
+             } catch (ClassNotFoundException e) {
+	           // TODO Auto-generated catch block
+	             e.printStackTrace();
+}
+	   }
+	   
+		void buyShowinfo(int choice) {
 
+			while(true) {
+				System.out.println("관리자 프로그램으로 접속합니다.");
+				System.out.println("관리자 id와 password를 입력해주세요.");
+				String id = sc.nextLine();
+			    String password = sc.nextLine();
+			    boolean isRightPass = (id.equals("hr") || password.equals("tiger"));
+			    if(id.equals(id) && isRightPass) {
+			    	System.out.println("성공입니다.");
+			    	break;
+			    }else {
+			    	System.out.println("id와 password가 틀렸습니다. 다시 입력해주세요");
+			    }
+			    
+			}
+
+		}
+			
 }
