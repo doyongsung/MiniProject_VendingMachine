bcfab800d02c493e28b5a40baf8d7a3578e341ff
kimrumm <83346234+kimrumm@users.noreply.github.com>
2021-06-23 15:16:42 +0900

    .

--- Vending/src/Ven/BuyOption2.java
+++ Vending/src/Ven/BuyOption2.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class BuyOption {
+public class BuyOption2 {
 	private Scanner name;
 	private Scanner price;
 
@@ -13,39 +13,29 @@ public class BuyOption {
 		ProductManager pManager = new ProductManager(ProductListDao.getInstance());
 
 		Scanner sc = new Scanner(System.in);
+		
+		// 데이터 베이스의 ProductList 테이블 이용 select 결과물 -> list 에 저장
+				Statement stmt = null;
+				ResultSet rs = null;
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
 			while (true) {
-				System.out.println();
 				System.out.println("---------- 아주시원한 자판기 -----------");
 				manager.buyitemList();
 				System.out.println("-----------------------------------");
 				System.out.println("원하시는 음료 번호를 입력해주세요.");
 				int num = Integer.parseInt(sc.nextLine());
 				System.out.println("-----------------------------------");
-
+				
 				// 콜라선택
-				if (num == 1) {
-					System.out.println("콜라를 선택하셨습니다.");
-					System.out.printf("구매수량을 입력해주세요 : ");
-					int qty = Integer.parseInt(sc.nextLine());
-					int total = qty * 700;
-					System.out.println("콜라 - " + total + "원 입니다.");
-					System.out.println("-----------------------------------");
-					System.out.println("1.추가구매 \t 2.종료 ");
-					int choice = Integer.parseInt(sc.nextLine());
-
-					if (choice == 1){
-						
-					} else {
-						System.out.println("감사합니다. 시원한 목축임 하세요 *^^*");
-						break;
-						
-					}
-				// 트랜젝션 처리하기 
-				// DB로 구매이력 insert -> 재고에 update
-
+				if(num == 1) {
+				System.out.println("콜라를 선택하셨습니다.");
+				System.out.printf("수량을 입력해주세요 : ");
+				int qty = Integer.parseInt(sc.next());
+				String sql = "select itemqty from PRODUCTINFO where name='콜라'";
+					
 				}
 			}
 
