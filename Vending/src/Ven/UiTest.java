d80e9489c865f5bb710578fcb5f0f3242cb7d8ef
kimrumm <83346234+kimrumm@users.noreply.github.com>
2021-06-25 15:17:17 +0900

    .

---
+++ Vending/src/Ven/UiTest.java
@@ -0,0 +1,16 @@
+package Ven;
+
+public class UiTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+		System.out.println("┃                                   ┃");
+		System.out.println("┃오늘도 저희 자판기를 찾아주셔서 감사합니다^^*  ┃");
+		System.out.println("┃━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┃ ");
+		System.out.println("┃음료번호 \t 음료이름 \t 음료가격 ");
+		System.out.println("┃-----------------------------------");
+		System.out.println("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+	}
+
+}
